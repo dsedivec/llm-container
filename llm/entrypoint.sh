ca52a45a4c386b019493943b9732b8f381e95902
Dale Sedivec <dale@codefu.org>
2025-12-04 19:02:03 -0600

    Add Copilot CLI

--- llm/entrypoint.sh
+++ llm/entrypoint.sh
@@ -46,6 +46,9 @@ if [ "$result" == 0 ]; then
 fi
 set -e
 
+install -d -o "$LLM_USER" -g "$LLM_USER" -m 0700 \
+        "$LLM_HOME_DIR/.persist/copilot"
+
 chown -hR "$LLM_USER:$LLM_USER" "$LLM_HOME_DIR"
 
 # runuser sets up the environment for us.  Otherwise we'd have to
